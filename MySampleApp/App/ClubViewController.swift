1d70ce88a591f889bbf839828e3819ead11b5193
Kira Ivarsson <kai2@princeton.edu>
2017-01-12 22:26:56 -0500

    changed the name of application to StreetSmart.  Some typing issues may exist, checking on other machines.

---
+++ MySampleApp/App/ClubViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ClubViewController.swift
+//  StreetStatus
+//
+//
+//
+
+import UIKit
+import AWSMobileHubHelper
+
+class ClubViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        self.view.addBackground()
+        
+    }
+    
+}
